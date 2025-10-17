c41dca7a999a2fcb24cdfffe2843de2aa0239cd5
Dylan P <dylan.m.priebe@gmail.com>
2025-10-17 17:05:50 -0500

    EWOK-86 Fixed server issues

--- backend/src/main/java/tastebase/api/security/SecurityConfig.java
+++ backend/src/main/java/tastebase/api/security/SecurityConfig.java
@@ -55,16 +55,4 @@ public class SecurityConfig {
                 .clientName("Google")
                 .build();
     }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 }
